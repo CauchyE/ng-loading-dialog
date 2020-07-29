d3c94da2d9cf6bba8be4a0fa079a2c47bff0cff1
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2020-07-29 12:51:31 +0900

    update

---
+++ projects/ng-loading-dialog/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-loading-dialog
+ */
+
+export * from './lib/ng-loading-dialog.service';
+export * from './lib/ng-loading-dialog.component';
+export * from './lib/ng-loading-dialog.module';
